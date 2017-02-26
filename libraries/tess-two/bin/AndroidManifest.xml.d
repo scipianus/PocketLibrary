537e82329954b131067897be35ad6146cf2c8638
scipianus <ciprian.olariu05@gmail.com>
2017-02-26 02:58:58 +0200

    Added Tesseract to the project and sample use

---
+++ libraries/tess-two/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Users\scipianus\Desktop\tess-two-master\tess-two-master\tess-two\bin\AndroidManifest.xml : \
+C:\Users\scipianus\Desktop\tess-two-master\tess-two-master\tess-two\AndroidManifest.xml \
